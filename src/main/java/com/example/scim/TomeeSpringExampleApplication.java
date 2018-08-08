231c75a3669789893eff8e38daf4905dd95f2168
Brian Demers <bdemers@apache.org>
2018-08-08 16:49:43 -0400

    Add work arounds, and try to cleanup dependency conflicts

--- src/main/java/com/example/scim/TomeeSpringExampleApplication.java
+++ src/main/java/com/example/scim/TomeeSpringExampleApplication.java
@@ -1,9 +1,11 @@
 package com.example.scim;
 
+import com.example.scim.scimple.workaround.NotImplementedSelfIdResolver;
 import org.apache.directory.scim.server.rest.ScimResourceHelper;
 
 import javax.ws.rs.ApplicationPath;
 import javax.ws.rs.core.Application;
+import java.util.HashSet;
 import java.util.Set;
 
 /**
@@ -13,17 +15,13 @@ import java.util.Set;
 public class TomeeSpringExampleApplication extends Application {
 
   public static void main(String[] args) {
-
-    // Work around issue executing this class from the command line
-    if (args == null || args.length == 0) {
-      args = new String[]{"--single-classloader"};
-    }
-
     org.apache.tomee.embedded.Main.main(args);
   }
 
   @Override
   public Set<Class<?>> getClasses() {
-    return ScimResourceHelper.getScimClassesToLoad();
+    Set<Class<?>> classes = new HashSet<>(ScimResourceHelper.getScimClassesToLoad());
+    classes.add(NotImplementedSelfIdResolver.class);
+    return classes;
   }
 }
